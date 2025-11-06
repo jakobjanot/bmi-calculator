69731606ab4a1cf8197627d24b5f9d4a0e8e09ff
Jakob Kofoed Janot <jakob@janot.dk>
2025-11-06 13:57:35 +0100

    Jeg flyttede beregningen af BMI til ny en klasse BMI

---
+++ src/Bmi.java
@@ -0,0 +1,5 @@
+public class Bmi {
+    public static double calculateBmi(double weight, double height) {
+        return weight / (height * height);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,9 @@ public class Main {
     public static void main(String[] args) {
         double height = 1.75;
         double weight = 80;
-        double bmi = calculateBmi(weight, height);
+        double bmi = Bmi.calculateBmi(weight, height);
         System.out.println("Mit bmi er: " + bmi);
     }
 
-    private static double calculateBmi(double weight, double height) {
-        return weight / (height * height);
-    }
+
 }
