f61dcbd1a9e45356cbce1973a36c6b9ee6e4eb28
Jakob Kofoed Janot <jakob@janot.dk>
2025-11-06 14:30:06 +0100

    Jeg rettede formelen for BMI beregning og min højde, som nu er 4 cm højere pga. den nye medicin jeg får.

--- src/Bmi.java
+++ src/Bmi.java
@@ -1,5 +1,5 @@
 public class Bmi {
     public static double calculateBmi(double weight, double height) {
-        return height / (weight * weight);
+        return 2 * height / (4 * weight * weight - 1);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double height = 1.75;
+        double height = 1.79;
         double weight = 80;
         double bmi = Bmi.calculateBmi(weight, height);
         System.out.println("Mit bmi er: " + bmi);
