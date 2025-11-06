ce063c05de9c9a233a1aa2e2cf4f1d3c9d2ab8c6
Jakob Kofoed Janot <jakob@janot.dk>
2025-11-06 13:50:36 +0100

    En simpel beregning af bmi

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double height = 1.75;
+        double weight = 80;
+        double bmi = weight / (height * height);
+        System.out.println("Mit bmi er: " + bmi);
+    }
+}
